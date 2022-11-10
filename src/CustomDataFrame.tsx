061c3faef25d0024b307bdcfcc3649ff187a51ad
MrYasuo <buihai0706vn@gmail.com>
2022-11-10 16:55:55 +0700

    fix comment input position

--- src/CustomDataFrame.tsx
+++ src/CustomDataFrame.tsx
@@ -6,14 +6,9 @@ import {
 } from "streamlit-component-lib";
 import { Table, TextInput, MantineProvider, Button } from "@mantine/core";
 import { CSVLink } from "react-csv";
-import React from "react";
 
-/**
- * We can use a Typescript interface to destructure the arguments from Python
- * and validate the types of the input
- */
 interface Data {
-	_field: string;
+	Field: string;
 	Min: number;
 	Max: number;
 	Group: string;
@@ -27,48 +22,24 @@ interface Data {
 	HHH: number;
 	Flag: number;
 	Evaluation: string;
-	comment?: string;
 }
 
-/**
- * No more props manipulation in the code.
- * We store props in state and pass value directly to underlying Slider
- * and then back to Streamlit.
- */
 declare type CustomComponentProps = Partial<ComponentProps>;
 
 const CustomDataFrame = (props: CustomComponentProps) => {
-	// Destructure using Typescript interface
-	// This ensures typing validation for received props from Python
-	// console.log(props.args);
 	let data: Data[] = [];
 	if (Object.keys(props).length > 0) {
 		data = props?.args?.data;
 	}
 
 	useEffect(() => Streamlit.setFrameHeight());
-	const [dataWithComments, setDataWithComments] = useState<Data[]>(data);
-	const handleSetDataWithComments = (value: string, index: number) => {
-		setDataWithComments((prev) => [
-			...prev.slice(0, index),
-			Object.assign({}, prev[index], { comment: value }),
-			...prev.slice(index + 1),
-		]);
-	};
+	const [comment, setComment] = useState("");
 
 	const rows = data.map((item, index) => (
 		<tr key={index}>
 			{Object.values(item).map((value, _index) => (
 				<td key={_index}>{value}</td>
 			))}
-			<td>
-				<TextInput
-					placeholder="Comment?"
-					value={dataWithComments[index]["comment"]}
-					onChange={(e) =>
-						handleSetDataWithComments(e.target.value, index)
-					}></TextInput>
-			</td>
 		</tr>
 	));
 
@@ -80,27 +51,29 @@ const CustomDataFrame = (props: CustomComponentProps) => {
 					light: ["E53E3E", "FFFFFF"],
 				},
 			}}>
-			<CSVLink data={dataWithComments} target="_blank" filename="data.csv">
+			<CSVLink
+				data={`Comment:,${comment}\n${Object.keys(data[0]).join(",")}\n${data
+					.map((item) => Object.values(item).join(","))
+					.join("\n")}`}
+				target="_blank"
+				filename="data.csv">
 				<Button variant="outline">Download</Button>
 			</CSVLink>
 			<Table highlightOnHover withBorder withColumnBorders>
 				<thead>
 					<tr>
-						<th>Field</th>
-						<th>Min</th>
-						<th>Max</th>
-						<th>Group</th>
-						<th>Description</th>
-						<th>Unit</th>
-						<th>LLL</th>
-						<th>LL</th>
-						<th>L</th>
-						<th>H</th>
-						<th>HH</th>
-						<th>HHH</th>
-						<th>Flag</th>
-						<th>Evaluation</th>
-						<th>Comment</th>
+						<th colSpan={Object.keys(data[0]).length}>
+							<TextInput
+								placeholder="Comment?"
+								value={comment}
+								onChange={(e) => setComment(e.target.value)}
+							/>
+						</th>
+					</tr>
+					<tr>
+						{Object.keys(data[0]).map((key, index) => (
+							<th key={index}>{key}</th>
+						))}
 					</tr>
 				</thead>
 				<tbody>{rows}</tbody>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CustomDataFrame from "./CustomDataFrame";
+import { config } from "dotenv";
+
+config();
 
 // Wrap your CustomDataFrame with the baseui them
 ReactDOM.render(
